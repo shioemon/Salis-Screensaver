b5bb7c2934954f91e7e587706edfe0f40bdb7427
島田誌音 <shionshimada0529@docomo.ne.jp>
2025-04-30 10:35:04 +0900

    DiscordのURLを修正し、https://を追加

--- Brooklyn/Enumerations/URLType.swift
+++ Brooklyn/Enumerations/URLType.swift
@@ -12,14 +12,22 @@ extension URLType {
     
     var urlString: String {
         switch self {
-            case .help: return "https://x.com/c_y_l_i"
-            case .twitter: return "https://x.com/c_y_l_i"
-            case .discord: return "discordapp.com/users/323041740963446785"
+        case .help: return "https://x.com/c_y_l_i"
+        case .twitter: return "https://x.com/c_y_l_i"
+        case .discord: return "https://discordapp.com/users/323041740963446785"
         }
     }
     
     func open() {
         guard let url = URL(string: urlString) else { return }
-        NSWorkspace.shared.open(url)
+        
+        // URLが開けるかチェック
+        if NSWorkspace.shared.open(url) == false {
+            // 開けなかった場合、デフォルトブラウザで開く試み
+            let browserURL = URL(string: "https://www.google.com/search?q=\(urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")")
+            if let browserURL = browserURL {
+                NSWorkspace.shared.open(browserURL)
+            }
+        }
     }
 }
